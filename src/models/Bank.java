eac91135da8e1a19028f71f51d5231d5f58278a5
Anders W. Olsen <anders@wiberg.tech>
2017-01-06 00:55:38 +0100

    Basic bank information
    This is only information needed to make a balance card.

--- src/models/Bank.java
+++ src/models/Bank.java
@@ -9,7 +9,16 @@ package models;/*
     /`           ´\                                      |
  */
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Bank
 {
+    private static List<BankAcct> bankAccts = new ArrayList<>();
 
+    public static BankAcct createBankAcct() {
+        BankAcct bankAcct = new BankAcct();
+        bankAccts.add(bankAcct);
+        return bankAcct;
+    }
 }
