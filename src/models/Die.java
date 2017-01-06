1bd37b2dcb945d3ff002cfa5bf5e88f510168b9d
trules <troelsjc@gmail.com>
2017-01-06 13:16:07 +0100

    Reused from CDIO_del3 develop branch on 2017-01-06

--- src/models/Die.java
+++ src/models/Die.java
@@ -9,7 +9,51 @@ package models;/*
     /`           ´\                                      |
  */
 
-public class Die  extends DiceCup
-{
+import java.util.Random;
 
+/**
+ * This class is a die that returns a random value.
+ * @version 1.0.2
+ * imported from CDIO_del3 develop branch on 2017-01-06.
+ */
+public class Die
+{
+    private int faceCount;
+    private int result;
+
+    private static Random rand;
+
+    public Die()
+    {
+        this.faceCount = 6;
+        result         = 0;
+        rand           = new Random();
+    }
+
+    public Die(int faceCount)
+    {
+        this.faceCount = faceCount;
+        result         = 0;
+        rand           = new Random();
+    }
+
+    public int getRolledDieResult()
+    {
+        roll();
+        return this.result;
+    }
+
+    public int getDieResult() {
+        return this.result;
+    }
+
+    public void roll() {
+        this.result = rand.nextInt(faceCount) + 1;
+    }
+
+    public void setFaceCount(int faceCount) {
+        this.faceCount = faceCount;
+    }
+
+    public int getFaceCount() { return this.faceCount; }
 }
