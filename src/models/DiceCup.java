3a4b70e0438640d7bdb1117167e68c772653c64d
trules <troelsjc@gmail.com>
2017-01-06 13:15:46 +0100

    Reused from CDIO_del3 develop branch on 2017-01-06

--- src/models/DiceCup.java
+++ src/models/DiceCup.java
@@ -9,7 +9,113 @@ package models;/*
     /`           ´\                                      |
  */
 
-public class DiceCup
-{
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
+/**
+ * This class is a die cup (a cup holding multiple dice. It basically handles all the dice in the game.
+ * @version 1.1.1
+ * imported from CDIO_del3 develop branch on 2017-01-06.
+ */
+public class DiceCup {
+    private Collection<Die> dice = new ArrayList<Die>();
+    private Collection<Integer> results = new ArrayList<Integer>();
+
+    private static int diceCount;
+    private static int faceCount;
+
+    public DiceCup() {
+        this.diceCount = 2;
+        this.faceCount = 6;
+        initDice();
+    }
+
+    public DiceCup(int diceCount) {
+        if (diceCount < 2)
+            throw new IndexOutOfBoundsException("There should be at least 2 dice.");
+        this.diceCount = diceCount;
+        this.faceCount = 6;
+        initDice();
+    }
+
+    public DiceCup(int diceCount, int faceCount) {
+        if (diceCount < 2)
+            throw new IndexOutOfBoundsException("There should be at least 2 dice.");
+        this.diceCount = diceCount;
+        this.faceCount = faceCount;
+        initDice();
+    }
+
+    public DiceCup(int diceCount, Die die) {
+        if (diceCount < 2)
+            throw new IndexOutOfBoundsException("There should be at least 2 dice.");
+        this.diceCount = diceCount;
+
+        initDice(die);
+    }
+
+    public DiceCup(Collection<Die> dice) {
+        if (dice.size() < 2)
+            throw new IndexOutOfBoundsException("There should be at least 2 dice.");
+        this.diceCount = dice.size();
+        initDice(dice);
+    }
+
+    private void initDice() {
+        for (int i = 0; i < diceCount; i++)
+            this.dice.add(new Die(faceCount));
+    }
+
+    private void initDice(Die die) {
+        for (int i = 0; i < diceCount; i++)
+            this.dice.add(die);
+    }
+
+    private void initDice(Collection<Die> dice) {
+        this.dice.addAll(dice);
+    }
+
+    public void addDice    (Die die)              { this.dice.add(die);        }
+    public void addDice    (Collection<Die> dice) { this.dice.addAll(dice);    }
+
+    public void removeDice (Die die)              { this.dice.remove(die);     }
+    public void removeDice (Collection<Die> dice) { this.dice.removeAll(dice); }
+
+    public void roll() {
+        this.results.clear();
+
+        for (Die die : dice)
+            this.results.add(die.getRolledDieResult());
+    }
+
+    public Collection<Integer> getResults() {
+        return results;
+    }
+
+    public Collection<Integer> getRolledDieResults() {
+        roll();
+        return results;
+    }
+
+    /**
+     * getTotalEyes returns the total number of eyes of the rolled dice.
+     * @return
+     */
+    public int getTotalEyes() {
+        int total = 0;
+
+        for (int i: this.getResults())
+            total += i;
+        return total;
+    }
+
+    public int[] getResultArr() {
+        int[] results = new int[2];
+        int i = 0;
+        for (Iterator<Integer> iter = this.getResults().iterator(); iter.hasNext(); i++)
+            results[i] = iter.next();
+        return results;
+    }
 }
+
