f4e64e66dba18e8017dcc274942b79e1ad1985ff
Anders W. Olsen <anders@wiberg.tech>
2017-01-07 18:27:26 +0100

    Creating useless unit test

---
+++ tests/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AndersWOlsen on 07-01-2017.
+ */
+public class MainTest {
+    @Test
+    public void main() throws Exception {
+        assertTrue(1 == 1);
+    }
+
+}
