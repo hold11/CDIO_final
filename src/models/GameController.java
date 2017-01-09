a43b84365763cdd5f7d9391cad4db151dca055f7
Anders W. Olsen <awo@defalt.net>
2017-01-09 17:27:48 +0100

    Working on the GameController

--- src/models/GameController.java
+++ src/models/GameController.java
@@ -9,7 +9,56 @@ package models;/*
     /`           ´\                                      |
  */
 
+import chanceCards.FreeBailCard;
+import chanceCards.OwnableCard;
+import fields.Jail;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameController
 {
+    private int playerTurn = 0;
 
+    public GameController() {
+
+    }
+
+    public void playTurn() {
+
+    }
+
+    public Jail.buttons[] getJailButtons() {
+        List<Jail.buttons> jailButtons = new ArrayList<>();
+
+        if (getCurrentPlayer().getPlayerAcct().getBalance() >= 1000) {
+            jailButtons.add(Jail.buttons.PAY_BAIL_OUT);
+        }
+        if (OwnableCard.playerHasCard(getCurrentPlayer(), FreeBailCard.class)) {
+            jailButtons.add(Jail.buttons.PAY_BAIL_OUT);
+        }
+
+        return jailButtons.toArray(new Jail.buttons[jailButtons.size()]);
+    }
+
+    public void nextPlayer() {
+        if (playerTurn + 1 < Player.getPlayers().size()) {
+            playerTurn++;
+        } else {
+            playerTurn = 0;
+        }
+    }
+
+    public int getPlayerTurn() {
+        return playerTurn;
+    }
+
+    public Player getCurrentPlayer() {
+        return Player.getPlayers().get(getPlayerTurn());
+    }
+
+    public Player getWinner() {
+        return new Player();
+
+    }
 }
