dbb2c2225306fdd76ebbf45c4adf6a06ce45ea49
VLC <valentinleonchristensen@gmail.com>
2017-01-10 12:01:59 +0100

    Comitting to update
    Also greated a file for the test Business

---
+++ tests/fields/BusinessTest.java
@@ -0,0 +1,69 @@
+package fields;
+
+import models.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by s165228 on 10-01-2017.
+ */
+public class BusinessTest {
+    @Before
+    public void setUp() throws Exception {
+        Player p1 = new Player();
+        Player p2 = new Player();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Player.reset();
+
+
+
+    }
+
+    @Test
+    public void landOnField() throws Exception {
+
+    }
+
+    @Test
+    public void landOnField1() throws Exception {
+
+    }
+
+    @Test
+    public void landOnField2() throws Exception {
+
+    }
+
+
+    @Test
+    public void getOwnedOwnables() throws Exception {
+
+    }
+
+    @Test
+    public void isOwned() throws Exception {
+
+    }
+
+    @Test
+    public void purchaseField() throws Exception {
+
+    }
+
+    @Test
+    public void getOwner() throws Exception {
+
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+
+    }
+
+}
