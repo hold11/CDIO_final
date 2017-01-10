523aaf3b1249b1a17a2761b509433a4b6586e3c8
VLC <valentinleonchristensen@gmail.com>
2017-01-10 12:29:03 +0100

    Ownable test done
    Test checking if field is ownable and if a field can be purchased
    multiple times.

--- tests/fields/BusinessTest.java
+++ tests/fields/BusinessTest.java
@@ -1,5 +1,6 @@
 package fields;
 
+import lang.Lang;
 import models.Player;
 import org.junit.After;
 import org.junit.Before;
@@ -11,58 +12,39 @@ import static org.junit.Assert.*;
  * Created by s165228 on 10-01-2017.
  */
 public class BusinessTest {
+    Player p1;
+    Player p2;
+    Business b1;
+    Business b2;
+
     @Before
     public void setUp() throws Exception {
-        Player p1 = new Player();
-        Player p2 = new Player();
+        Lang.setLanguage(new String[] {"da", "DK"});
+        p1 = new Player();
+        p2 = new Player();
+        b1 = new Business(1, 1000);
+        b2 = new Business(2, 2000);
     }
 
     @After
     public void tearDown() throws Exception {
         Player.reset();
-
-
-
     }
 
     @Test
-    public void landOnField() throws Exception {
-
+    public void testIsOwnable() throws Exception {
+        b1.purchaseField(p1);
+        assertTrue(b1.isOwned());
+    }
+    @Test
+    public void testOnlyOneOwner() throws Exception {
+        b1.purchaseField(p2);
+        assertEquals(p1, b1.getOwner());
     }
 
     @Test
-    public void landOnField1() throws Exception {
-
-    }
-
-    @Test
-    public void landOnField2() throws Exception {
-
-    }
-
-
-    @Test
-    public void getOwnedOwnables() throws Exception {
-
-    }
-
-    @Test
-    public void isOwned() throws Exception {
-
-    }
-
-    @Test
-    public void purchaseField() throws Exception {
-
-    }
-
-    @Test
-    public void getOwner() throws Exception {
-
-    }
-
-    @Test
-    public void getPrice() throws Exception {
+    public void testLandOnField() throws Exception {
+        p1.landPlot(1, 2, 1000, 2000, 3000);
 
     }
 
